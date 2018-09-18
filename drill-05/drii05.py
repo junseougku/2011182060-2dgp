acdb9d0f90661eeeb3215c15f91178c5776fa245
2011182060-jsg <jsg3001kr@daum.net>
2018-09-18 20:41:53 +0900

    캐릭터가 움직이는 함수를 구현
    캐릭터가 움직이는 함수를 만들었습니다

--- drill-05/drii05.py
+++ drill-05/drii05.py
@@ -3,8 +3,32 @@ from pico2d import *
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+character = load_image('animation_sheet.png')
 
+def movePosition(_x,_y):
+    x = 800//2
+    y = 600//2
+    frame = 0
+    moveX = (_x - x) / 30
+    moveY = (_y - y) / 30
+    count = 0
+    while (True):
+        clear_canvas()
+        grass.draw(400, 30)
+        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        update_canvas()
+        frame = (frame + 1) % 8
+        x += moveX
+        y += moveY
+        if(count ==30):
+            break;
+        count += 1
+        delay(0.05)
+        get_events()
+
+while(True):
+    movePosition(203 , 535)
+    movePosition(132 , 243)
 
 
 close_canvas()
