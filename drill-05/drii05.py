0a3e5905e24d9a50984864e2c388a00fc3cddfa4
2011182060-jsg <jsg3001kr@daum.net>
2018-09-18 20:54:03 +0900

    캐릭터가 원하는좌표로 쳐다보면서 걸어감
    변수 이름까지 모두 바꾸고 갑니다.

--- drill-05/drii05.py
+++ drill-05/drii05.py
@@ -5,19 +5,24 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('animation_sheet.png')
 
-def movePosition(_x,_y,x,y):
+def movePosition(_x,_y,now_x,now_y):
     frame = 0
-    moveX = (_x - x) / 30
-    moveY = (_y - y) / 30
+    moveX = (_x - now_x) / 30
+    moveY = (_y - now_y) / 30
     count = 0
+    rot = 0
+    if(now_x - _x >0) :
+        rot = 0
+    else :
+        rot = 100
     while (True):
         clear_canvas()
         grass.draw(400, 30)
-        character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        character.clip_draw(frame * 100, rot, 100, 100, now_x, now_y)
         update_canvas()
         frame = (frame + 1) % 8
-        x += moveX
-        y += moveY
+        now_x += moveX
+        now_y += moveY
         if(count ==30):
             break;
         count += 1
@@ -25,8 +30,17 @@ def movePosition(_x,_y,x,y):
         get_events()
 
 while(True):
-    movePosition(203 , 535,800//2,600//2)
+    movePosition(203 , 535,712,349)
     movePosition(132 , 243,203,535)
+    movePosition(535,470,132,243)
+    movePosition(477,203,535,470)
+    movePosition(715,136,477,203)
+    movePosition(316,225,715,136)
+    movePosition(510,92,316,225)
+    movePosition(692,518,510,92)
+    movePosition(682,336,692,518)
+    movePosition(712,349,682,336)
+
 
 
 close_canvas()
