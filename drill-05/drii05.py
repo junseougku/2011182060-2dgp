279ff1f7af034eda46fdf15b3f131ae68730c2fc
hogumin_BuildingC <hojiny9923@gmail.com>
2018-09-18 17:49:27 +0900

    코드의 기본 구조 완성
    코드의 기본 구조의 역할이 네모를 그리고 원을 그리는거다 뼈대를 완성한것이다.

--- drill-05/drii05.py
+++ drill-05/drii05.py
@@ -5,6 +5,11 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+def make_rectangle():
+    pass
+def make_circle():
+    pass
+
 while True:
     make_rectangle()
     make_circle()
