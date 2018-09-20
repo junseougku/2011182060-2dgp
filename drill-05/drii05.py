674e42e4c561c5bc6c16a19f6b0628eb366d7232
2011182060-jsg <jsg3001kr@daum.net>
2018-09-20 18:41:52 +0900

    drill-06 구조 마련
    배경이미지를 띄우고 캐릭터가 왔다갔다함

--- drill-05/drii05.py
+++ drill-05/drii05.py
@@ -5,6 +5,22 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
+character.draw_now(203,535)
+def move_position(_x,_y):
+     px =203
+     py = 535
+    while(True):
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now( px, py)
+        px -=1
+        py -=1
+        delay(0.01)
+        if(_x == px and _y == py):
+            break;
 
+
+while True:
+    move_position(132,243);
 
 close_canvas()
